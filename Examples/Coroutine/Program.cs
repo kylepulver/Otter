e4305473ed3eebe666c42852280059317c98d459
Will Sams <willsams@hotmail.com>
2020-04-06 19:48:54 -0400

    Modified examples to allow Game object to call Dispose when it goes out of scope.

--- Examples/Coroutine/Program.cs
+++ Examples/Coroutine/Program.cs
@@ -3,9 +3,11 @@
 namespace CoroutineExample {
     class Program {
         static void Main(string[] args) {
-            var game = new Game();
-            game.FirstScene = new CoroutineScene();
-            game.Start();
+            using(var game = new Game())
+            {
+                game.FirstScene = new CoroutineScene();
+                game.Start();
+            }
         }
     }
 }
