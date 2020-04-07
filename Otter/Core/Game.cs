f5b2f6030ef7dd63b8fc5567b17006b8c870ee43
Will Sams <willsams@hotmail.com>
2020-04-06 21:24:21 -0400

    Minor code aesthetic fix.

--- Otter/Core/Game.cs
+++ Otter/Core/Game.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 using SFML.Graphics;
 using SFML.Window;
 using SFML.System;
-using System.Runtime.InteropServices;
 
 namespace Otter
 {
